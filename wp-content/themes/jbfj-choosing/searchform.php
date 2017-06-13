af18ecc0ed5fb618fa910de7a2d2fcb4b6e46101
jedifunk <iam@bryceflory.com>
2017-06-12 21:59:44 -0500

    header nav updates

--- wp-content/themes/jbfj-choosing/searchform.php
+++ wp-content/themes/jbfj-choosing/searchform.php
@@ -2,7 +2,7 @@
 ?>
 <form role="search" method="get" id="searchform" class="searchform" action="/">
 	<div>
-		<input type="text" value="" name="s" id="s" placeholder="Search for ..." />
 		<button type="submit" id="searchsubmit"><i class="fa fa-search"></i></button>
+		<input type="text" value="" name="s" id="s" placeholder="Search for ..." />
 	</div>
 </form>
