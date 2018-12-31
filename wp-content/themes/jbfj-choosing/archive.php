06a9e4e269eb0678ebb3363dabda13f299007e26
Bryce Flory <bflory@go-yamamoto.com>
2018-12-31 11:21:06 -0600

    WP 5 update, search and archive setup

--- wp-content/themes/jbfj-choosing/archive.php
+++ wp-content/themes/jbfj-choosing/archive.php
@@ -11,14 +11,16 @@ get_header(); ?>
 
 	<div id="primary" class="content-area wrapper">
 		<header class="page-header">
+    		    <h1 class="page-title">
 			<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
+				//the_archive_title( '<h1 class="page-title">', '</h1>' );
+				single_term_title();
 			?>
+    		    </h1>
 		</header><!-- .page-header -->
 
 		<div class="grid-wrapper">
-			<main id="main" class="site-main" role="main">
+			<main class="content inner-grid">
 
 			<?php
 			if ( have_posts() ) : ?>
@@ -27,15 +29,7 @@ get_header(); ?>
 				/* Start the Loop */
 				while ( have_posts() ) : the_post();
 
-					if ( is_category( 'snapshots' ) ) :
-
-						get_template_part( 'template-parts/content', 'snapshot' );
-
-					else :
-
-						get_template_part( 'template-parts/content', get_post_format() );
-
-					endif;
+				    get_template_part( 'template-parts/content', 'simple' );
 
 				endwhile;
 
